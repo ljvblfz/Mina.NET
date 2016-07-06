e5eda186e38559129e4a66ae7ff42f160f3749af
longshine <longxianghe@gmail.com>
2016-07-06 13:46:32 +0800

    Format code.

--- Mina.NET/Core/Service/IOServiceSupport.cs
+++ Mina.NET/Core/Service/IOServiceSupport.cs
@@ -5,9 +5,13 @@ namespace Mina.Core.Service
     interface IOServiceSupport
     {
         void FireServiceActivated();
+
         void FireServiceIdle(IdleStatus idleStatus);
+
         void FireSessionCreated(IOSession session);
+
         void FireSessionDestroyed(IOSession session);
+
         void FireServiceDeactivated();
     }
 }
