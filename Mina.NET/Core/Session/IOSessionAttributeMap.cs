68102bb4ff524cda0eadf24e8bb9551f67fcdf48
longshine <longxianghe@gmail.com>
2016-07-06 13:58:43 +0800

    Format code.

--- Mina.NET/Core/Session/IOSessionAttributeMap.cs
+++ Mina.NET/Core/Session/IOSessionAttributeMap.cs
@@ -16,27 +16,33 @@ namespace Mina.Core.Session
         /// returned.
         /// </summary>
         object GetAttribute(IOSession session, object key, object defaultValue);
+
         /// <summary>
         /// Sets a user-defined attribute.
         /// </summary>
         object SetAttribute(IOSession session, object key, object value);
+
         /// <summary>
         /// Sets a user defined attribute if the attribute with the specified key
         /// is not set yet.
         /// </summary>
         object SetAttributeIfAbsent(IOSession session, object key, object value);
+
         /// <summary>
         /// Removes a user-defined attribute with the specified key.
         /// </summary>
         object RemoveAttribute(IOSession session, object key);
+
         /// <summary>
         /// Returns <tt>true</tt> if this session contains the attribute with the specified <tt>key</tt>.
         /// </summary>
         bool ContainsAttribute(IOSession session, object key);
+
         /// <summary>
         /// Returns the keys of all user-defined attributes.
         /// </summary>
         IEnumerable<object> GetAttributeKeys(IOSession session);
+
         /// <summary>
         /// Disposes any releases associated with the specified session.
         /// This method is invoked on disconnection.
