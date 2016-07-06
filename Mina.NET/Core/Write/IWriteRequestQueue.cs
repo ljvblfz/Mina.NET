838cbe594f7eb63f78e43b3ded058dedea060873
longshine <longxianghe@gmail.com>
2016-07-06 14:03:38 +0800

    Format code.

--- Mina.NET/Core/Write/IWriteRequestQueue.cs
+++ Mina.NET/Core/Write/IWriteRequestQueue.cs
@@ -13,29 +13,34 @@ namespace Mina.Core.Write
         /// <param name="session">the associated session</param>
         /// <returns>the first available request, if any.</returns>
         IWriteRequest Poll(IOSession session);
+
         /// <summary>
         /// Adds a new WriteRequest to the session write's queue
         /// </summary>
         /// <param name="session">the associated session</param>
         /// <param name="writeRequest">the writeRequest to add</param>
         void Offer(IOSession session, IWriteRequest writeRequest);
+
         /// <summary>
         /// Tells if the WriteRequest queue is empty or not for a session.
         /// </summary>
         /// <param name="session">the associated session</param>
         /// <returns><code>true</code> if the writeRequest queue is empty</returns>
         bool IsEmpty(IOSession session);
+
         /// <summary>
         /// Removes all the requests from this session's queue.
         /// </summary>
         /// <param name="session">the associated session</param>
         void Clear(IOSession session);
+
         /// <summary>
         /// Disposes any releases associated with the specified session.
         /// This method is invoked on disconnection.
         /// </summary>
         /// <param name="session">the associated session</param>
         void Dispose(IOSession session);
+
         /// <summary>
         /// Gets number of objects currently stored in the queue.
         /// </summary>
