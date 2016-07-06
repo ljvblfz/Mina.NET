e9b463fa7523694a4542ae7b19a1def2295af54b
longshine <longxianghe@gmail.com>
2016-07-06 11:48:28 +0800

    Format code.

--- Mina.NET/Core/Filterchain/INextFilter.cs
+++ Mina.NET/Core/Filterchain/INextFilter.cs
@@ -13,38 +13,47 @@ namespace Mina.Core.Filterchain
         /// Forwards <code>SessionCreated</code> event to next filter.
         /// </summary>
         void SessionCreated(IOSession session);
+
         /// <summary>
         /// Forwards <code>SessionOpened</code> event to next filter.
         /// </summary>
         void SessionOpened(IOSession session);
+
         /// <summary>
         /// Forwards <code>SessionClosed</code> event to next filter.
         /// </summary>
         void SessionClosed(IOSession session);
+
         /// <summary>
         /// Forwards <code>SessionIdle</code> event to next filter.
         /// </summary>
         void SessionIdle(IOSession session, IdleStatus status);
+
         /// <summary>
         /// Forwards <code>ExceptionCaught</code> event to next filter.
         /// </summary>
         void ExceptionCaught(IOSession session, Exception cause);
+
         /// <summary>
         /// Forwards <code>InputClosed</code> event to next filter.
         /// </summary>
         void InputClosed(IOSession session);
+
         /// <summary>
         /// Forwards <code>MessageReceived</code> event to next filter.
         /// </summary>
         void MessageReceived(IOSession session, object message);
+
         /// <summary>
         /// Forwards <code>MessageSent</code> event to next filter.
         /// </summary>
         void MessageSent(IOSession session, IWriteRequest writeRequest);
+
         /// <summary>
         /// Forwards <code>FilterClose</code> event to next filter.
         /// </summary>
         void FilterClose(IOSession session);
+
         /// <summary>
         /// Forwards <code>FilterWrite</code> event to next filter.
         /// </summary>
