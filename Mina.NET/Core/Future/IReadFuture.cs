5e3e8976775cea2342d4ad835b9528e24baec391
longshine <longxianghe@gmail.com>
2016-07-06 13:29:46 +0800

    Format code.

--- Mina.NET/Core/Future/IReadFuture.cs
+++ Mina.NET/Core/Future/IReadFuture.cs
@@ -17,21 +17,25 @@ namespace Mina.Core.Future
         /// All threads waiting for will be notified while being set.
         /// </remarks>
         object Message { get; set; }
+
         /// <summary>
         /// Returns <code>true</code> if a message was received successfully.
         /// </summary>
         bool Read { get; }
+
         /// <summary>
         /// Gets or sets a value indicating if the <see cref="IOSession"/>
         /// associated with this future has been closed.
         /// </summary>
         bool Closed { get; set; }
+
         /// <summary>
         /// Gets or sets the cause of the read failure if and only if the read
         /// operation has failed due to an <see cref="Exception"/>.
         /// Otherwise null is returned.
         /// </summary>
         Exception Exception { get; set; }
+
         /// <inheritdoc/>
         new IReadFuture Await();
     }
