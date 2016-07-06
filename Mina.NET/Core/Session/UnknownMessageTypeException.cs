6f054b1e61c7029aed330a68f863475679848176
longshine <longxianghe@gmail.com>
2016-07-06 14:01:13 +0800

    Format code.

--- Mina.NET/Core/Session/UnknownMessageTypeException.cs
+++ Mina.NET/Core/Session/UnknownMessageTypeException.cs
@@ -12,21 +12,29 @@ namespace Mina.Core.Session
     {
         /// <summary>
         /// </summary>
-        public UnknownMessageTypeException() { }
+        public UnknownMessageTypeException()
+        {
+        }
 
         /// <summary>
         /// </summary>
         public UnknownMessageTypeException(string message)
-            : base(message) { }
+            : base(message)
+        {
+        }
 
         /// <summary>
         /// </summary>
         public UnknownMessageTypeException(string message, Exception inner)
-            : base(message, inner) { }
+            : base(message, inner)
+        {
+        }
 
         /// <summary>
         /// </summary>
         protected UnknownMessageTypeException(SerializationInfo info, StreamingContext context)
-            : base(info, context) { }
+            : base(info, context)
+        {
+        }
     }
 }
