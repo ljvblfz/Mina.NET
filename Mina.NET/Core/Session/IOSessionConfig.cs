d30d25dbca9761bcc82395fb4563146b5bb5c933
longshine <longxianghe@gmail.com>
2016-07-06 13:59:13 +0800

    Format code.

--- Mina.NET/Core/Session/IOSessionConfig.cs
+++ Mina.NET/Core/Session/IOSessionConfig.cs
@@ -14,48 +14,59 @@
         /// </remarks>
         /// </summary>
         int ReadBufferSize { get; set; }
+
         /// <summary>
         /// Gets or sets the interval (seconds) between each throughput calculation.
         /// The default value is 3 seconds.
         /// </summary>
         int ThroughputCalculationInterval { get; set; }
+
         /// <summary>
         /// Returns the interval (milliseconds) between each throughput calculation.
         /// The default value is 3 seconds.
         /// </summary>
         long ThroughputCalculationIntervalInMillis { get; }
+
         /// <summary>
         /// Returns idle time for the specified type of idleness in seconds.
         /// </summary>
         int GetIdleTime(IdleStatus status);
+
         /// <summary>
         /// Returns idle time for the specified type of idleness in milliseconds.
         /// </summary>
         long GetIdleTimeInMillis(IdleStatus status);
+
         /// <summary>
         /// Sets idle time for the specified type of idleness in seconds.
         /// </summary>
         void SetIdleTime(IdleStatus status, int idleTime);
+
         /// <summary>
         /// Gets or sets idle time for <see cref="IdleStatus.ReaderIdle"/> in seconds.
         /// </summary>
         int ReaderIdleTime { get; set; }
+
         /// <summary>
         /// Gets or sets idle time for <see cref="IdleStatus.WriterIdle"/> in seconds.
         /// </summary>
         int WriterIdleTime { get; set; }
+
         /// <summary>
         /// Gets or sets idle time for <see cref="IdleStatus.BothIdle"/> in seconds.
         /// </summary>
         int BothIdleTime { get; set; }
+
         /// <summary>
         /// Gets or set write timeout in seconds.
         /// </summary>
         int WriteTimeout { get; set; }
+
         /// <summary>
         /// Gets write timeout in milliseconds.
         /// </summary>
         long WriteTimeoutInMillis { get; }
+
         /// <summary>
         /// Sets all configuration properties retrieved from the specified config.
         /// </summary>
