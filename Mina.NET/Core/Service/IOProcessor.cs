69a8d41b15cbee2654db32d72036655e471ac195
longshine <longxianghe@gmail.com>
2016-07-06 13:42:22 +0800

    Format code.

--- Mina.NET/Core/Service/IOProcessor.cs
+++ Mina.NET/Core/Service/IOProcessor.cs
@@ -16,17 +16,20 @@ namespace Mina.Core.Service
         /// </summary>
         /// <param name="session">the session to add</param>
         void Add(IOSession session);
+
         /// <summary>
         /// Writes the <see cref="IWriteRequest"/> for the specified <see cref="IOSession"/>.
         /// </summary>
         /// <param name="session">the session where the message to be written</param>
         /// <param name="writeRequest">thr message to write</param>
         void Write(IOSession session, IWriteRequest writeRequest);
+
         /// <summary>
         /// Flushes the internal write request queue of the specified <see cref="IOSession"/>.
         /// </summary>
         /// <param name="session">the session to flush</param>
         void Flush(IOSession session);
+
         /// <summary>
         /// Removes and closes the specified <see cref="IOSession"/> from the I/O
         ///  processor so that the I/O processor closes the connection
@@ -35,6 +38,7 @@ namespace Mina.Core.Service
         /// </summary>
         /// <param name="session">the session to remove</param>
         void Remove(IOSession session);
+
         /// <summary>
         /// Controls the traffic of the specified <paramref name="session"/>
         /// depending of the <see cref="IOSession.ReadSuspended"/>
@@ -59,17 +63,20 @@ namespace Mina.Core.Service
         /// </summary>
         /// <param name="session">the session to add</param>
         void Add(TS session);
+
         /// <summary>
         /// Writes the <see cref="IWriteRequest"/> for the specified <typeparamref name="TS"/>.
         /// </summary>
         /// <param name="session">the session we want the message to be written</param>
         /// <param name="writeRequest">the message to write</param>
         void Write(TS session, IWriteRequest writeRequest);
+
         /// <summary>
         /// Flushes the internal write request queue of the specified <typeparamref name="TS"/>.
         /// </summary>
         /// <param name="session">the session to flush</param>
         void Flush(TS session);
+
         /// <summary>
         /// Removes and closes the specified <typeparamref name="TS"/> from the I/O
         ///  processor so that the I/O processor closes the connection
@@ -78,6 +85,7 @@ namespace Mina.Core.Service
         /// </summary>
         /// <param name="session">the session to remove</param>
         void Remove(TS session);
+
         /// <summary>
         /// Controls the traffic of the specified <paramref name="session"/>
         /// depending of the <see cref="IOSession.ReadSuspended"/>
