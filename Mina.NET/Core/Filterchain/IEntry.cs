13ecf23eaa9c7b8f5c6424da090b78c8843a3777
longshine <longxianghe@gmail.com>
2016-07-06 11:47:56 +0800

    Format code.

--- Mina.NET/Core/Filterchain/IEntry.cs
+++ Mina.NET/Core/Filterchain/IEntry.cs
@@ -9,26 +9,32 @@
         /// Gets the name of the filter.
         /// </summary>
         string Name { get; }
+
         /// <summary>
         /// Gets the filter.
         /// </summary>
         TFilter Filter { get; }
+
         /// <summary>
         /// Gets the <typeparamref name="TNextFilter"/> of the filter.
         /// </summary>
         TNextFilter NextFilter { get; }
+
         /// <summary>
         /// Adds the specified filter with the specified name just before this entry.
         /// </summary>
         void AddBefore(string name, TFilter filter);
+
         /// <summary>
         /// Adds the specified filter with the specified name just after this entry.
         /// </summary>
         void AddAfter(string name, TFilter filter);
+
         /// <summary>
         /// Replace the filter of this entry with the specified new filter.
         /// </summary>
         void Replace(TFilter newFilter);
+
         /// <summary>
         /// Removes this entry from the chain it belongs to.
         /// </summary>
