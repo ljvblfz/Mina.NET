57f6830cec11c1d6567ae9cb68daaac6d7c18f20
longshine <longxianghe@gmail.com>
2016-07-06 13:47:05 +0800

    Format code.

--- Mina.NET/Core/Service/ITransportMetadata.cs
+++ Mina.NET/Core/Service/ITransportMetadata.cs
@@ -11,21 +11,25 @@ namespace Mina.Core.Service
         /// Gets the name of the service provider.
         /// </summary>
         string ProviderName { get; }
+
         /// <summary>
         /// Gets the name of the service.
         /// </summary>
         string Name { get; }
+
         /// <summary>
         /// Returns <code>true</code> if the session of this transport type is
         /// <a href="http://en.wikipedia.org/wiki/Connectionless">connectionless</a>.
         /// </summary>
         bool Connectionless { get; }
+
         /// <summary>
         /// Returns <code>true</code> if the messages exchanged by the service can be
         /// <a href="http://en.wikipedia.org/wiki/IPv4#Fragmentation_and_reassembly">fragmented
         /// or reassembled</a> by its underlying transport.
         /// </summary>
         bool HasFragmentation { get; }
+
         /// <summary>
         /// Gets the type of the endpoint in this transport.
         /// </summary>
