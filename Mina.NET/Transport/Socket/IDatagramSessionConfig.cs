193a9994840e7e62d1e7def0f19c7117bee22eac
longshine <longxianghe@gmail.com>
2016-07-07 15:03:36 +0800

    Format code.

--- Mina.NET/Transport/Socket/IDatagramSessionConfig.cs
+++ Mina.NET/Transport/Socket/IDatagramSessionConfig.cs
@@ -1,4 +1,5 @@
-﻿using Mina.Core.Session;
+﻿using System.Net.Sockets;
+using Mina.Core.Session;
 
 namespace Mina.Transport.Socket
 {
@@ -11,29 +12,35 @@ namespace Mina.Transport.Socket
         /// <see cref="System.Net.Sockets.Socket.EnableBroadcast"/>
         /// </summary>
         bool? EnableBroadcast { get; set; }
+
         /// <summary>
         /// <see cref="System.Net.Sockets.Socket.ExclusiveAddressUse"/>
         /// </summary>
         bool? ExclusiveAddressUse { get; set; }
+
         /// <summary>
         /// Gets or sets if <see cref="System.Net.Sockets.SocketOptionName.ReuseAddress"/> is enabled.
         /// </summary>
         bool? ReuseAddress { get; set; }
+
         /// <summary>
         /// <see cref="System.Net.Sockets.Socket.ReceiveBufferSize"/>
         /// </summary>
         int? ReceiveBufferSize { get; set; }
+
         /// <summary>
         /// <see cref="System.Net.Sockets.Socket.SendBufferSize"/>
         /// </summary>
         int? SendBufferSize { get; set; }
+
         /// <summary>
         /// Gets or sets traffic class or <see cref="System.Net.Sockets.SocketOptionName.TypeOfService"/> in the IP datagram header.
         /// </summary>
         int? TrafficClass { get; set; }
+
         /// <summary>
         /// Gets or sets <see cref="System.Net.Sockets.MulticastOption"/>.
         /// </summary>
-        System.Net.Sockets.MulticastOption MulticastOption { get; set; }
+        MulticastOption MulticastOption { get; set; }
     }
 }
