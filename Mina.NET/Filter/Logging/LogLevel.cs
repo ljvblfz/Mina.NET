884f3a7bebfd7b1b1ab573b7a8ea774143d01aa0
longshine <longxianghe@gmail.com>
2016-07-07 14:08:00 +0800

    Format code.

--- Mina.NET/Filter/Logging/LogLevel.cs
+++ Mina.NET/Filter/Logging/LogLevel.cs
@@ -9,22 +9,27 @@
         /// Not log any information
         /// </summary>
         None,
+
         /// <summary>
         /// Logs messages on the ERROR level.
         /// </summary>
         Error,
+
         /// <summary>
         /// Logs messages on the WARN level.
         /// </summary>
         Warn,
+
         /// <summary>
         /// Logs messages on the INFO level.
         /// </summary>
         Info,
+
         /// <summary>
         /// Logs messages on the DEBUG level.
         /// </summary>
         Debug,
+
         /// <summary>
         /// Logs messages on the TRACE level.
         /// </summary>
