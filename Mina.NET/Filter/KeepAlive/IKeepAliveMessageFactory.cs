3b037c9e4874dfda5bfe611cc87e7f3392d7b2de
longshine <longxianghe@gmail.com>
2016-07-07 14:02:09 +0800

    Format code.

--- Mina.NET/Filter/KeepAlive/IKeepAliveMessageFactory.cs
+++ Mina.NET/Filter/KeepAlive/IKeepAliveMessageFactory.cs
@@ -12,16 +12,19 @@ namespace Mina.Filter.KeepAlive
         /// keep-alive request message.
         /// </summary>
         bool IsRequest(IOSession session, object message);
+
         /// <summary>
         /// Returns <tt>true</tt> if and only if the specified message is a 
         /// keep-alive response message;
         /// </summary>
         bool IsResponse(IOSession session, object message);
+
         /// <summary>
         /// Returns a (new) keep-alive request message.
         /// Returns <tt>null</tt> if no request is required.
         /// </summary>
         object GetRequest(IOSession session);
+
         /// <summary>
         /// Returns a (new) response message for the specified keep-alive request.
         /// Returns <tt>null</tt> if no response is required.
